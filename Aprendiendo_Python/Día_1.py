8c233532d1011bd38fd2c6ac5825537dcdc34a57
ivankr <ivankr6@gmail.com>
2023-11-18 10:59:50 -0300

    Día 1 v.0.3

--- Aprendiendo_Python/Día_1.py
+++ Aprendiendo_Python/Día_1.py
@@ -7,13 +7,22 @@
 Con comillas se puede hacer comentarios, si no esta sujeto a alguna variable o print.
 Se puede usar para comentarios de n lineas.
 """
+'''
+Con comillas simples tambien se puede comentar n lineas
+'''
 # No hace falta agregar el ';' al final
 
 # Imprime Hola python; se puede usar comillas dobles o simples para los prints.
+
 print("Hola python")
 print('Hola python')
 
+# La funcion 'type()' me dice el tipo de dato.
 
+print(type("Hola python"))   # tipo 'str' string.
 
+print(type(5))   # tipo 'int' número entero.
 
+print(type(1.2))   # tipo 'float' número flotante.
 
+print(type(True))   # tipo 'bool' True o False.
