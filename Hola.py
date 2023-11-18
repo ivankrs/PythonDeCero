1b5459bb177645f3846160e505a1b58f10c01ef2
ivankr <ivankr6@gmail.com>
2023-11-18 10:20:28 -0300

    dia 1 en pytthon

---
+++ Hola.py
@@ -0,0 +1,19 @@
+# Día 1 Aprendiendo Python:
+
+
+# El '#' se usa para comentarios de una linea
+
+"""
+Con comillas se puede hacer comentarios, si no esta sujeto a alguna variable o print.
+Se puede usar para comentarios de n lineas.
+"""
+# No hace falta agregar el ';' al final
+
+# Imprime Hola python; se puede usar comillas dobles o simples para los prints.
+print("Hola python")
+print('Hola python')
+
+
+
+
+
